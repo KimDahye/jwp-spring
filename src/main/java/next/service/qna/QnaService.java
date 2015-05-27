858d4599272f5c81d29bf4022a6740ec023b6748
Dahye Kim <dahye.kim@nhnnext.org>
2015-05-27 20:07:15 +0900

    8th pb solved. transaction added.

--- src/main/java/next/service/qna/QnaService.java
+++ src/main/java/next/service/qna/QnaService.java
@@ -9,14 +9,22 @@ import next.dao.qna.QuestionDao;
 import next.model.qna.Answer;
 import next.model.qna.Question;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Service
 @Scope("prototype")
 public class QnaService {
 	@Resource(name = "questionDao")
 	private QuestionDao questionDao;
+	
+	@Autowired
+	private DataSourceTransactionManager transactionManager;
 	
 	@Resource(name = "answerDao")
 	private AnswerDao answerDao;
@@ -48,17 +56,28 @@ public class QnaService {
 	}
 
 	public void addAnswer(long questionId, Answer answer) {
+		TransactionStatus status = getTransactionStatus();
 		answer.setQuestionId(questionId);
 		answerDao.insert(answer);
 		questionDao.increaseCommentCount(questionId);
+		transactionManager.commit(status);
 	}
 
 	public void deleteAnswer(long questionId, long answerId) {
+		TransactionStatus status = getTransactionStatus();
 		answerDao.delete(answerId);
 		questionDao.decreaseCommentCount(questionId);
+		transactionManager.commit(status);
 	}
 
 	public void update(Question question, long id) {
 		questionDao.update(question, id);
+	}
+	
+	private TransactionStatus getTransactionStatus() {
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		def.setName("example-transaction");
+		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		return transactionManager.getTransaction(def);
 	}
 }
